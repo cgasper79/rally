94b1fd5e1a77ab3d27b11dcc1826952ad8ae16cf
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2021-02-04 14:02:45 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
   $('#coche').load('coche.html');
   $('#participa').load('participa.html');
 
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -45,7 +46,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#services').magnificPopup({
+  $('#2').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Cargando imagen #%curr%...',
