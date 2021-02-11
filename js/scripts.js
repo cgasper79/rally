23cf1e6a65eca36b665798f0d6218567e6e2eecd
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2021-02-11 09:55:14 +0100

    Añadido mensaje cookies

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,8 @@
 (function($) {
   "use strict"; // Start of use strict
   
-  //cargamos coche y participación
-  $('#coche').load('coche.html');
+  //cargamos participación
   $('#participa').load('participa.html');
-
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -77,3 +75,53 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// Cookies
+
+window.addEventListener('load', function(){
+  window.cookieconsent.initialise({
+   revokeBtn: "<div class='cc-revoke'></div>",
+   type: "opt-in",
+   theme: "classic",
+   palette: {
+       popup: {
+           background: "#6c757d",
+           opacity: "0.5",
+           text: "#fff"
+        },
+       button: {
+           background: "#fe5",
+           text: "#000"
+        }
+    },
+   content: {
+       message: "Nuestra Web utiliza cookies para que tengas la mejor experiencia de usuario.",
+       link: "Cookies",
+       allow: "Permitir",
+       deny: "Cancelar",
+       href: "https://2gdpr.com/cookies"
+    },
+    onInitialise: function(status) {
+      if(status == cookieconsent.status.allow) myScripts();
+    },
+    onStatusChange: function(status) {
+      if (this.hasConsented()) myScripts();
+    }
+  })
+});
+
+
+function myScripts() {
+
+   // Paste here your scripts that use cookies requiring consent. See examples below
+
+   /* Google Analytics, you need to change 'UA-00000000-1' to your ID
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-00000000-1', 'auto');
+      ga('send', 'pageview');
+ */
+}
